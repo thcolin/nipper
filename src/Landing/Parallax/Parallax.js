091f2ba708e020280c369a59349b8a4d377e9580
thcolin <th.colin3@gmail.com>
2017-03-03 20:00:04 +0100

    Fix Parallax with Rellax lib

--- src/Landing/Parallax/Parallax.js
+++ src/Landing/Parallax/Parallax.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import { css } from 'aphrodite'
 
+import Rellax from 'rellax'
+
 import styles from './Parallax.styles'
 
 class Parallax extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount(){
+    this.state.rellax = new Rellax('.' + css(styles.global));
+  }
+
   render(){
     return(
       <div className={css(styles.global)}>
