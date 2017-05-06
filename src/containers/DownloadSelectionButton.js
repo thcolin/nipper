fa8763be574ae2d32334b670c9966bc09ee6caf8
thcolin <th.colin3@gmail.com>
2017-05-07 01:13:21 +0200

    Fix DownloadSelectionButton progress when downloading a single video

--- src/containers/DownloadSelectionButton.js
+++ src/containers/DownloadSelectionButton.js
@@ -5,7 +5,7 @@ import Button from 'components/Shared/Button'
 const mapStateToProps = (state) => ({
   icon: (state.context.downloading ? 'fa-circle-o-notch fa-spin fa-fw':''),
   badge: (Object.keys(state.videos).filter(id => state.videos[id].selected).length ? Object.keys(state.videos).filter(id => state.videos[id].selected).length:null),
-  progress: (Object.keys(state.videos).map(id => state.videos[id]).filter(video => video.selected).map(video => video.progress).reduce((accumulator, progress) => accumulator + progress, 0) / Object.keys(state.videos).filter(id => state.videos[id].selected).length),
+  progress: (state.context.downloading ? Object.keys(state.videos).map(id => state.videos[id]).filter(video => video.selected).map(video => video.progress).reduce((accumulator, progress) => accumulator + progress, 0) / Object.keys(state.videos).filter(id => state.videos[id].selected).length : 0),
   children: (state.context.downloading ? 'Cancel':(Object.keys(state.videos).filter(id => state.videos[id].selected).length ? 'Download Selection':'Select some videos')),
   disabled: (!Object.keys(state.videos).filter(id => state.videos[id].selected).length)
 })
